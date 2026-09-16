425b842283c4350bc59ddda3923ea3e58ae29890
Xushnud Eshtemirov <xushnud.eshtemirov@aab.uz>
2026-09-16 18:13:26 +0500

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import { Link } from "react-router-dom";
 
 import { NAV_LINKS } from "../data/portfolio";
 import { useClock } from "../hooks/useClock";
